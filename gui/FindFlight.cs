02363dd405074d011e831e03bfe75f9ce6121d76
eliaset <simply753@gmail.com>
2020-02-02 14:39:07 -0500

    Agregados los componentes básicos de la interfaz de usuario.

--- gui/FindFlight.cs
+++ gui/FindFlight.cs
@@ -16,5 +16,17 @@ namespace gui
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gmap_Load(object sender, EventArgs e)
+        {
+            gmap.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            gmap.SetPositionByKeywords("Paris, France");
+        }
     }
 }
